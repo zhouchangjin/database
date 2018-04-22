bc4abf7f97556d29d2466d8cb9afb0736efa4073
Administrator <Administrator@www.myass.com>
2018-04-22 22:34:45 +0800

    bo

--- src/main/java/com/gamewolf/database/entity/BasicObject.java
+++ src/main/java/com/gamewolf/database/entity/BasicObject.java
@@ -1,30 +1,49 @@
 package com.gamewolf.database.entity;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.HashMap;
+import com.alibaba.fastjson.JSON;
+
 public class BasicObject implements JSONEntity, XMLEntity {
 	
 	public JSONVendor jsonVendor;
 	public XMLVendor xmlVendor;
 	public Object baseObject;
+	HashMap<String,Object> properties;
+	
 	
 	public BasicObject(Object baseObject,JSONVendor jsonVendor,XMLVendor xmlVendor) {
+
+		properties=new HashMap<String,Object>();
 		this.jsonVendor=jsonVendor;
 		this.xmlVendor=xmlVendor;
+		
 	}
 	
 	private BasicObject() {
-		
+		properties=new HashMap<String,Object>();
 	}
 
 	@Override
 	public String toXMLString() {
 		// TODO Auto-generated method stub
-		return jsonVendor.fromObject(this);
+		return xmlVendor.fromObject(this);
 	}
 
 	@Override
 	public String toJSONString() {
 		// TODO Auto-generated method stub
-		return xmlVendor.fromObject(this);
+		if(jsonVendor==null) {
+			jsonVendor=new JSONVendor() {
+				
+				@Override
+				public String fromObject(Object obj) {
+					return JSON.toJSONString(properties);
+				}
+			};
+		}
+		return jsonVendor.fromObject(this);
 	}
 	
 	public static BasicObject buildBasicObject() {
@@ -46,6 +65,69 @@ public class BasicObject implements JSONEntity, XMLEntity {
 	public BasicObject setBaseObject(Object obj) {
 		this.baseObject=obj;
 		return this;
+	}
+	
+	public BasicObject set(String name,Object value) {
+		this.properties.put(name, value);
+		return this;
+	}
+	
+	public Object get(String name) {
+		if(this.properties.containsKey(name)) {
+			return this.properties.get(name);
+		}else {
+			try {
+				Method m=this.baseObject.getClass().getMethod("get"+name.substring(0,1).toUpperCase()+name.substring(1));
+				Object value= m.invoke(this.baseObject);
+				this.properties.put(name, value);
+				return value;
+			} catch (NoSuchMethodException e) {
+				e.printStackTrace();
+				return null;
+			} catch (SecurityException e) {
+				e.printStackTrace();
+				return null;
+			} catch (IllegalAccessException e) {
+				e.printStackTrace();
+				return null;
+			} catch (IllegalArgumentException e) {
+				e.printStackTrace();
+				return null;
+			} catch (InvocationTargetException e) {
+				e.printStackTrace();
+				return null;
+			}
+		}
+	}
+	
+	public BasicObject build(){
+		
+		Method[] ms=this.baseObject.getClass().getMethods();
+		for(Method m:ms) {
+			if(m.getName().startsWith("get") && m.getParameterCount()==0) {
+				String propertyName=m.getName().substring(3, 4).toLowerCase()+m.getName().substring(4);
+				Object value;
+				try {
+					value = m.invoke(baseObject);
+				} catch (IllegalAccessException e) {
+					e.printStackTrace();
+					continue;
+				} catch (IllegalArgumentException e) {
+					e.printStackTrace();
+					continue;
+				} catch (InvocationTargetException e) {
+					e.printStackTrace();
+					continue;
+				}
+				properties.put(propertyName, value);
+			}
+		}
+		return this;
+	}
+	
+	
+	public static void main(String args[]) {
+
 	}
 	
 
