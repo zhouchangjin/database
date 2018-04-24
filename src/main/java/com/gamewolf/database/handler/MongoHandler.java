b0a1bc472581b68978e005a7421b3cce99e94a2c
Administrator <Administrator@www.myass.com>
2018-04-24 21:39:38 +0800

    update mongo

--- src/main/java/com/gamewolf/database/handler/MongoHandler.java
+++ src/main/java/com/gamewolf/database/handler/MongoHandler.java
@@ -1,18 +1,27 @@
 package com.gamewolf.database.handler;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 
+import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.core.MongoOperations;
 import org.springframework.data.mongodb.core.query.BasicQuery;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
 import com.gamewolf.database.dbconnector.ClientManager;
 import com.gamewolf.database.dbconnector.ClientProxy;
 import com.gamewolf.database.dbconnector.MongoClientProxy;
 import com.gamewolf.database.dbsource.MongoDBDataSource;
 import com.gamewolf.database.entity.BasicObject;
+import com.gamewolf.database.util.JSONUtil;
+import com.mongodb.BasicDBList;
 import com.mongodb.BasicDBObject;
+import com.mongodb.DBCursor;
+import com.mongodb.DBObject;
 
 public class MongoHandler implements IDatasourceHandler<MongoDBDataSource> {
 	
@@ -71,26 +80,76 @@ public class MongoHandler implements IDatasourceHandler<MongoDBDataSource> {
 		List<Object> list=new ArrayList<Object>();
 		BasicQuery bq=new BasicQuery("{}") ;
 		bq.skip(0).limit(10);
-		List<BasicDBObject> mylist=this.op.find(bq,BasicDBObject.class );
-		for(BasicDBObject object:mylist) {
-			BasicObject bo=buildBasicObject(object);
+		DBCursor cursor=this.op.getCollection(this.getDatasource().getCollection()).find();
+		while(cursor.hasNext()) {
+			DBObject obj=cursor.next();
+			BasicObject bo=buildBasicObject(obj);
 			list.add(bo);
 		}
 		return list;
 	}
 	
+	private void processAlibabaJSONArray(JSONArray array,BasicDBList list) {
+		for(Object obj:array) {
+			if(obj instanceof JSONObject) {
+				BasicDBObject mongoObj=new BasicDBObject();
+				processAlibabaJSONObject((JSONObject) obj,mongoObj);
+				list.add(mongoObj);
+			}else if(obj instanceof JSONArray) {
+				BasicDBList mongoList=new BasicDBList();
+				processAlibabaJSONArray((JSONArray)obj, mongoList);
+				list.add(mongoList);
+			}else{
+				list.add(obj);
+			}
+		}
+	}
+	private void processAlibabaJSONObject(JSONObject jsonObj,BasicDBObject mongoObj) {
+		Iterator<String>propIt=jsonObj.keySet().iterator();
+		while(propIt.hasNext()) {
+			String prop=propIt.next();
+			Object value=jsonObj.get(prop);
+			if(value instanceof JSONObject) {
+				BasicDBObject basicDBObject=new BasicDBObject();
+				mongoObj.put(prop, basicDBObject);
+				processAlibabaJSONObject((JSONObject)value, basicDBObject);
+			}else if(value instanceof JSONArray) {
+				JSONArray array=(JSONArray)value;
+				BasicDBList dbList=new BasicDBList();
+				mongoObj.put(prop, dbList);
+				processAlibabaJSONArray(array, dbList);
+			}else {
+				mongoObj.put(prop, value);
+			}
+		}
+	}
 	public void insert(Object obj) {
-		this.op.insert(obj);
+		BasicDBObject db=new BasicDBObject();
+		ObjectId id=new ObjectId();
+		db.put("_id", id);
+		if(obj instanceof String) {
+			String str=(String)obj;
+			JSONObject json=JSON.parseObject(str);
+			processAlibabaJSONObject(json, db);
+		}else if(obj instanceof JSONObject){
+			JSONObject json=(JSONObject)obj;
+			processAlibabaJSONObject(json, db);
+		}else if(obj instanceof HashMap) {
+			
+		}else {
+			
+		}
+		this.op.getCollection(this.getDatasource().getCollection()).insert(db);
 	}
 
-	private BasicObject buildBasicObject(BasicDBObject object) {
+	private BasicObject buildBasicObject(DBObject object) {
 		BasicObject bo=BasicObject.buildBasicObject();
 		Iterator<String> it=object.keySet().iterator();
 		while(it.hasNext()) {
 			String key=it.next();
 			Object obj=object.get(key);
-			if(obj instanceof BasicDBObject) {
-				BasicDBObject bdb=(BasicDBObject)obj;
+			if(obj instanceof DBObject) {
+				DBObject bdb=(DBObject)obj;
 				bo.set(key, buildBasicObject(bdb));
 			}else {
 				bo.set(key, obj);
