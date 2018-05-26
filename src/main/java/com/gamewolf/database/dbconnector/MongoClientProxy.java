07cfb13ff1e26f768194087ef21e6ae88b38f6aa
Gitea <gitea@fake.local>
2018-05-26 17:03:31 +0800

    登录密码

--- src/main/java/com/gamewolf/database/dbconnector/MongoClientProxy.java
+++ src/main/java/com/gamewolf/database/dbconnector/MongoClientProxy.java
@@ -2,6 +2,9 @@ package com.gamewolf.database.dbconnector;
 
 
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.data.mongodb.MongoDbFactory;
 import org.springframework.data.mongodb.core.MongoOperations;
 import org.springframework.data.mongodb.core.MongoTemplate;
@@ -10,6 +13,9 @@ import org.springframework.data.mongodb.core.SimpleMongoDbFactory;
 import com.gamewolf.database.dbsource.AbstractDataSource;
 import com.gamewolf.database.dbsource.MongoDBDataSource;
 import com.mongodb.MongoClient;
+import com.mongodb.MongoClientOptions;
+import com.mongodb.MongoCredential;
+import com.mongodb.ServerAddress;
 
 public class MongoClientProxy extends ClientProxy{
 	
@@ -25,8 +31,19 @@ public class MongoClientProxy extends ClientProxy{
 			MongoDBDataSource mdb=(MongoDBDataSource)abstractDatasource;
 			String ip=mdb.getHost();
 			int port=mdb.getPort();
-			MongoClient client=new MongoClient(ip,port);
-			MongoDbFactory factory=new SimpleMongoDbFactory(client, mdb.getDatabase());
+			String userName=mdb.getUsername();
+			String password=mdb.getPassword();
+			//MongoClientOptions options=new MongoClientOptions.Builder().build();
+			MongoClient mongoClient=null;
+			if(userName!=null && !userName.equals("")) {
+				MongoCredential credential = MongoCredential.createMongoCRCredential(userName, mdb.getDatabase(), password.toCharArray());
+				List<MongoCredential> list=new ArrayList<MongoCredential>();
+				list.add(credential);
+				mongoClient = new MongoClient(new ServerAddress(ip, port), list);
+			}else {
+				mongoClient=new MongoClient(ip,port);
+			}
+			MongoDbFactory factory=new SimpleMongoDbFactory(mongoClient, mdb.getDatabase());
 			operation=new MongoTemplate(factory);
 		}
 	}
